2f2f388354c2156c6f7a0b9d187786cde59459f9
MohamedSarifa <mohamedsarifa2007@gmail.com>
2026-04-10 11:38:52 +0530

    Add files via upload

---
+++ prb2.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def display(self):
+        print(f"Title: {self.title}, Author: {self.author}")
+a=Book("Harry Potter","JKRowling")
+a.display()
